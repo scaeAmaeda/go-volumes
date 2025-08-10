458ad1df49e498eaad4c2f33a58b5edd1a9acfa4
scaeAmaeda <pmg1843@gmail.com>
2025-08-10 20:27:19 +0200

    up to date and iso

--- go.mod
+++ go.mod
@@ -2,4 +2,25 @@ module main
 
 go 1.23.4
 
-require github.com/pdfcpu/pdfcpu v0.11.0 // indirect
+require (
+	github.com/pdfcpu/pdfcpu v0.11.0
+	github.com/pgaskin/kepubify/v4 v4.0.4
+)
+
+require (
+	github.com/beevik/etree v1.1.0 // indirect
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/pkcs7 v0.2.0 // indirect
+	github.com/hhrutter/tiff v1.0.2 // indirect
+	github.com/kr/smartypants v0.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/pgaskin/kepubify/_/go116-zip.go117 v0.0.0-20210611152744-2d89b3182523 // indirect
+	github.com/pgaskin/kepubify/_/html v0.0.0-20211223234002-6ee2cc632cdc // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/image v0.27.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,86 @@
 package main
 
 import (
+	"archive/zip"
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
+	"github.com/pgaskin/kepubify/v4/kepub"
 )
 
 func main() {
-	truc, err := os.ReadDir(".\\chapitres")
+	fgr := ""
+	for i := 1; i < len(os.Args); i++ {
+		fgr = fgr + os.Args[i] + "_"
+	}
+
+	os.Mkdir(".\\workingdir", os.ModePerm)
+	temp := ".\\workingdir"
+	defer os.RemoveAll(temp)
+	var nameBook string = temp + "\\" + fgr + ".pdf"
+	var nameBookEpub string = temp + "\\" + fgr + ".epub"
+	var nameBookKepub string = ".\\" + fgr + ".kepub.epub"
+	conf := model.NewDefaultConfiguration()
+	chapters, err := os.ReadDir(".\\chapters")
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, f := range truc {
+	for _, f := range chapters {
 		if f.IsDir() {
-			var pa string = ".\\chapitres\\" + f.Name()
-			fi, _ := os.ReadDir(pa)
+			var path string = ".\\chapters\\" + f.Name()
+			fi, _ := os.ReadDir(path)
+			allChap := []string{}
 			for _, g := range fi {
-				fmt.Println(g.Name())
+				localFile := ".\\chapters\\" + f.Name() + "\\" + g.Name()
+				allChap = append(allChap, localFile)
 			}
+			outputPDF := temp + "\\" + f.Name() + ".pdf"
+			api.ImportImagesFile(allChap, outputPDF, nil, conf)
 		}
-		fmt.Println(f.Name())
 	}
+	// tout est dans le temp il faut concat
+	allChapPDF := []string{}
+	tempDir, _ := os.ReadDir(temp)
+	for _, c := range tempDir {
+		locFile := temp + "\\" + c.Name()
+		allChapPDF = append(allChapPDF, locFile)
+	}
+	api.MergeCreateFile(allChapPDF, nameBook, false, nil)
+	// ici virer le sommaire qui se fout parce qu'on est pas en CLI
+	ctxPDF, _ := api.ReadContextFile(nameBook)
+	totalPages := ctxPDF.PageCount + 1
+	pagesToRemove := fmt.Sprintf("%d", totalPages)
+	api.RemovePagesFile(nameBook, nameBook, []string{pagesToRemove}, conf)
+
+	convertiToEpub(nameBook, nameBookEpub)
+
+	ctx := context.Background()
+	// 0 securité, fuck la sécurité on est pas en entreprise
+	epubFile, _ := os.Open(nameBookEpub)
+	defer epubFile.Close()
+	stat, _ := epubFile.Stat()
+	size := stat.Size()
+	zipReader, _ := zip.NewReader(epubFile, size)
+	outFile, _ := os.Create(nameBookKepub)
+	defer outFile.Close()
+	conv := kepub.NewConverter()
+	err = conv.Convert(ctx, outFile, zipReader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.RemoveAll(".\\chapters")
+	os.Mkdir(".\\chapters", os.ModePerm)
+
+}
+
+func convertiToEpub(input string, output string) error {
+	cmd := exec.Command("ebook-convert", input, output)
+	err := cmd.Run()
+	return err
 }
